1cbe136c32ecb74f14c8305f3f704a9aa843bdd3
Adja18 <fatimatabder@gmail.com>
2025-04-07 15:47:54 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,29 +1,29 @@
+
+
+    
+    
+
+
+
 from flask import Flask
 from flask import render_template
-from flask import json
-app = Flask(_name_)
+from flask import json                                                                                                                                     
+app = Flask(__name__)                                                                                                                  
 
 @app.route('/<path:valeurs>')
 def exercice(valeurs):
     liste_nombres = valeurs.split('/')
     liste_nombres = [int(n) for n in liste_nombres]
-
-    max_valeur = liste_nombres[0]
+    
+    min_valeur = liste_nombres[0]
     for n in liste_nombres:
-        if n > max_valeur:
-            max_valeur = n
+        if n < min_valeur:
+            min_valeur = n
 
-    return str(max_valeur)
+    return str(min_valeur)
 
-if name == '_main_':
+if __name__ == '__main__':
     app.run(debug=True)
-
-    
-    
-
-
-
-
 
 
 
