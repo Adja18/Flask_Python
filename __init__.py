3368b56e37ef549d65a70ae2865b913f64342805
Adja18 <fatimatabder@gmail.com>
2025-04-07 10:14:14 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,15 +5,20 @@ from flask import json
 app = Flask(__name__)                                                                                                                  
 
 
-pyramide= ''
-for i in range(1, valeur +1):
-    pyramide += '&nbsp;' * (valeur - i)
-    for j in range(1, i + 1):
-        pyramide +=str(j)
-        for j in range(i -1, 0, -1):
-            pyramide +=str(j)
-            pyramide +='<br>'
-            return pyramide
+    @app.route('/<int:valeur>')
+def exercice(valeur):
+    pyramide = ''
+    for i in range(1, valeur + 1):
+        # Espaces pour centrer
+        pyramide += '&nbsp;' * (valeur - i)
+        # Chiffres qui montent
+        for j in range(1, i + 1):
+            pyramide += str(j)
+        # Chiffres qui descendent
+        for j in range(i - 1, 0, -1):
+            pyramide += str(j)
+        pyramide += '<br>'
+    return pyramide
 
 
 
