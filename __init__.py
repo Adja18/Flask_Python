de90cc51c454e226fbaecac136afddf1a038fac3
Adja18 <fatimatabder@gmail.com>
2025-04-07 12:30:29 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,17 +5,34 @@ app = Flask(__name__)
 
 @app.route('/<int:valeur>')
 def exercice(valeur):
-    a, b = 0, 1
-    sequence = [a, b]
-    for _ in range(2, valeur):
-        a, b = b, a + b
-        sequence.append(b)
+    # Demande à l'utilisateur de saisir une valeur pour n
+    n= int(input("entrer un nombre n:"))
 
-    fibonacci = '<pre>' + ', '.join(map(str, sequence)) + '</pre>'
-    return fibonacci
+    # Initialisation de la variable somme à 0
+    somme = 0
+
+    # Boucle sur les nombres de 1 à n
+for i in range(1, n+1):
+
+    # vérifie si le nombre est divisible par 5 ou par 7
+    if i % 5 == 0 or i % 7 == 0:
+        # Si le nombre est divisible par 11, on passe au suivant
+        if i% 11 == 0:
+            continue
+        # Ajoute le nombre à la somme
+          somme+= i
+        # Si la somme dépasse 5000, on arrête immédiatement la boucle
+        if somme > 5000:
+            break
+
+# Affiche la somme finale
+print("la somme finale est:4886", somme)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+  app.run(debug=True)
+    
+    
+
 
 
 
