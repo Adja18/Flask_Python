a498012c2e354943f0fd17bae3240c23b79d766a
Adja18 <fatimatabder@gmail.com>
2025-04-07 12:55:19 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,35 +1,16 @@
-from flask import Flask
-from flask import render_template
-from flask import json                                                                                                                                     
-app = Flask(__name__)                                                                                                                  
-
 @app.route('/<int:valeur>')
 def exercice(valeur):
-    # Demande à l'utilisateur de saisir une valeur pour n
-    n= int(input("entrer un nombre n:"))
-
-    # Initialisation de la variable somme à 0
     somme = 0
-
-    # Boucle sur les nombres de 1 à n
-for i in range(1, n+1):
-
-    # vérifie si le nombre est divisible par 5 ou par 7
-    if i % 5 == 0 or i % 7 == 0:
-        # Si le nombre est divisible par 11, on passe au suivant
-        if i% 11 == 0:
+    for i in range(1, valeur + 1):
+        if i % 11 == 0:
             continue
-        # Ajoute le nombre à la somme
-          somme+= i
-        # Si la somme dépasse 5000, on arrête immédiatement la boucle
-        if somme > 5000:
-            break
+        if i % 5 == 0 or i % 7 == 0:
+            if somme + i > 5000:
+                break
+            somme += i
+    
+    return f'<pre>Somme finale: {somme}</pre>'
 
-# Affiche la somme finale
-print("la somme finale est:4886", somme)
-
-if __name__ == "__main__":
-  app.run(debug=True)
     
     
 
